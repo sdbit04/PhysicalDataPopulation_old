5cecd95ffafbbe91f2f5be9d69959caf45e77d0e
swapan <sdbit04@gmail.com>
2019-12-30 19:25:45 +0530

    Included band information into lookup

--- Physical_data_population/profile_reader.py
+++ Physical_data_population/profile_reader.py
@@ -6,6 +6,34 @@ class ProfileReader(object):
     def __init__(self, antenna_profile_directory):
         self.profile_root_path = antenna_profile_directory
 
+    def read_profile(self, profile_path_p):
+        profile_dict = {}
+        profile_path = profile_path_p
+        with open(profile_path, 'r', newline='') as profile_ob:
+            for line in profile_ob.readlines():
+                line_item = line.split(' ')
+                # print("{}".format(line_item[0]))
+                if line_item[0] == 'ELECTRICAL_TILT':
+                    profile_dict[line_item[0]] = line_item[1]
+                elif line_item[0] == 'FREQUENCY':
+                    profile_dict[line_item[0]] = line_item[1]
+            return profile_dict
+
+    def get_band_for_a_frequency(self, frequency):
+        frequency_input = frequency
+        band = 900
+        if 2100 <= frequency_input <= 2199:
+            band = 2100
+        elif 850 <= frequency_input <= 999:
+            band = 900
+        elif 1700 <= frequency_input <= 1899:
+            band = 1800
+        elif 2300 <= frequency_input <= 2399:
+            band = 2300
+        else:
+            band = 900
+        return band
+
     def create_antenna_model_vs_profile_map(self):
         antenna_model_vs_profile_map = {}
         antenna_models_list = os.listdir(self.profile_root_path)
@@ -19,28 +47,24 @@ class ProfileReader(object):
                 profile_path_abs = os.path.join(model_dir_abs, profile)
                 antenna_model_profile_combination = "{}/{}".format(model_dir, profile.rstrip(".txt"))
                 # print(antenna_model_profile_combination)
-                ELECTRICAL_TILT =self.read_profile(profile_path_abs)['ELECTRICAL_TILT']
+                profile_file_data_dict = self.read_profile(profile_path_abs)
+                # print(profile_file_data_dict)
+                band = self.get_band_for_a_frequency(float(profile_file_data_dict['FREQUENCY']))
+                # ELECTRICAL_TILT_band = "{}-{}".format(profile_file_data_dict['ELECTRICAL_TILT'], band)
+                # antenna_model_eTilt_combination = "{}/{}".format(model_dir, ELECTRICAL_TILT_band)
+                ELECTRICAL_TILT = "{}".format(profile_file_data_dict['ELECTRICAL_TILT'])
                 antenna_model_eTilt_combination = "{}/{}".format(model_dir, ELECTRICAL_TILT)
+
                 try:
+                    # just to check if there is exception to get value, we are not using value1 identifier in future.
                     value1 = antenna_model_vs_profile_map[antenna_model_eTilt_combination]
                 except KeyError:
                     antenna_model_vs_profile_map[antenna_model_eTilt_combination] = antenna_model_profile_combination
-                else:
-                    print("Key {} is already exist, overwriting by new value".format(antenna_model_eTilt_combination))
-                    antenna_model_vs_profile_map[antenna_model_eTilt_combination] = antenna_model_profile_combination
+                # else:
+                #     print("Key {} is already exist, overwriting by new value".format(antenna_model_eTilt_combination))
+                #     antenna_model_vs_profile_map[antenna_model_eTilt_combination] = antenna_model_profile_combination
 
         return antenna_model_vs_profile_map
-
-    def read_profile(self, profile_path_p):
-        profile_dict = {}
-        profile_path = profile_path_p
-        with open(profile_path, 'r', newline='') as profile_ob:
-            for line in profile_ob.readlines():
-                line_item = line.split(' ')
-                # print("{}".format(line_item[0]))
-                if line_item[0] == 'ELECTRICAL_TILT':
-                    profile_dict[line_item[0]] = line_item[1]
-                    return profile_dict
 
 
 if __name__ == "__main__":
@@ -48,12 +72,5 @@ if __name__ == "__main__":
     profile_reader = ProfileReader(profile_root_path)
     # profile_reader.create_antenna_model_vs_profile_map()
     antenna_model_vs_profile_map = profile_reader.create_antenna_model_vs_profile_map()
-
     print(antenna_model_vs_profile_map)
-
-
-
-
-
-
 
