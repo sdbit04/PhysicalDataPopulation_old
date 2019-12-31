e48e2b4818f01faeaae7f5f46b6e67dac9bff58a
sdbit04 <sdbit04@gmail.com>
2019-12-31 18:31:23 +0530

    SGI and lte carrier file look-up implemented

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        entry_points={'console_scripts': ['phyDataPopulation = Physical_data_population.read_CLI_n_run:main_method']}
-
+        entry_points={'console_scripts': ['phyDataPopulation = Physical_data_population.read_CLI_n_run:main_method']},
+        install_requires=['pyxlsb', 'selenium']
     )
