8990f0dd5dd155b27aa1c09ab2db933ba24fc5bc
sdbit04 <sdbit04@gmail.com>
2020-01-16 15:23:37 +0530

    Added dependency as offline available

--- setup.py
+++ setup.py
@@ -2,8 +2,5 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        entry_points={'console_scripts': ['phyDataPopulation = Physical_data_population.read_CLI_n_run:main_method']}, install_requires=['pyxlsb', 'xlrd']
-
-
-
+        entry_points={'console_scripts': ['phyDataPopulation = Physical_data_population.read_CLI_n_run:main_method']}
     )
