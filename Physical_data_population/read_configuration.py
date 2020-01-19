b58f45993cd69c935a75438482a253d88c173976
SwapanDas-Personal <sdbit04@gmail.com>
2020-01-19 16:13:52 +0530

    Updated configuration file reader to check each parameter, also check if planning or GIS, which file has been provided. Or both has been provided, return a string along with json object.

--- Physical_data_population/read_configuration.py
+++ Physical_data_population/read_configuration.py
@@ -4,59 +4,65 @@ import time
 
 def read_configuration(config_path_p):
     config_path = config_path_p
+    planning_or_gis = ""
     with open(config_path, 'r') as config_ob:
         try:
             config_json_ob = json.load(config_ob)
         except json.decoder.JSONDecodeError:
-            # raise json.decoder.JSONDecodeError("""Please make sure you have "sd_path","planning_file", "out_put_data_dict_dir","profile_root_path" with their value at config_phy.ini file, each key and value should be in " " """)
             print("""Please check config_phy.ini file, make sure you have "sd_path","planning_file", "out_put_data_dict_dir","profile_root_path" with their value, each key and value should be in " ",
             Example: 
-            {"technology":"UMTS"
-            "sd_path_csv" :"D:\\Input_data_deep\\Antennas_sd.txt",
-            "planning_file_csv" :"D:\\Input_data_deep\\Planning_input.txt",
-            "lte_carrier_file_csv":"D:\\Input_data_deep\\lte_carrier.txt",
+            {"technology":"LTE",
+            "Network_directory_path": "D:\\D_drive_BACKUP\\Study\\PycharmProjects\\PhysicalDataPopulation\\Input_data_deep\\Network",
+            "Directory_names_for_NE": "ZTE_LTE_KOL_24, ZTE_LTE_KOL_25",
+            "planning_file_csv" :"D:\\Input_data_deep\\Planning_input.txt",            
             "GSI_file_xlsb" :"D:\\Input_data_deep\\SGI_input.xlsb",
-            "out_put_data_dict_dir" :"D:\\out_dir",
             "profile_root_path": "D:\\Input_data_deep\\Ant Model"
+            "out_put_data_dict_dir" :"D:\\out_dir",
             }""")
             time.sleep(1)
             raise json.decoder.JSONDecodeError
         else:
             try:
                 technology = config_json_ob["technology"]
-                Network_directory_path = config_json_ob["Network_directory_path"]
-                planning_file = config_json_ob["planning_file_csv"]
-                Directory_names_for_NE = config_json_ob["Directory_names_for_NE"]
-                CGI_file = config_json_ob["GSI_file_xlsb"]
-                profile_root_path = config_json_ob["profile_root_path"]
-                out_put_data_dict_dir = config_json_ob["out_put_data_dict_dir"]
-
                 print(technology)
+            except KeyError as technology_e:
+                raise technology_e
+            try:
+                Network_directory_path = config_json_ob["Network_directory_path"]
                 print(Network_directory_path)
-                print(planning_file)
+            except KeyError as Network_directory_path_e:
+                raise Network_directory_path_e
+            try:
+                Directory_names_for_NE = config_json_ob["Directory_names_for_NE"]
                 print(Directory_names_for_NE)
+            except KeyError as Directory_names_for_NE_e:
+                raise Directory_names_for_NE_e
+            try:
+                planning_file = config_json_ob["planning_file_csv"]
+                print(planning_file)
+            except KeyError as planning_file_e:
+                planning_or_gis = "{}{}".format(planning_or_gis, 'NP')
+                print(planning_file_e)
+            try:
+                CGI_file = config_json_ob["GSI_file_xlsb"]
                 print(CGI_file)
-                print(out_put_data_dict_dir)
+            except KeyError as CGI_file_e:
+                planning_or_gis = "{}{}".format(planning_or_gis, 'NG')
+                print(CGI_file_e)
+            try:
+                profile_root_path = config_json_ob["profile_root_path"]
                 print(profile_root_path)
-            except KeyError:
-                # raise KeyError("""Please make sure you have "sd_path","planning_file", "out_put_data_dict_dir","profile_root_path" with their value at config_phy.ini file, each key and value should be in " " """)
-                print("""Please check config_phy.ini file, make sure you have "sd_path","planning_file", "out_put_data_dict_dir","profile_root_path" with their value, each key and value should be in " ",
-                           Example: 
-                           {"technology":"UMTS"
-                            "sd_path_csv" :"D:\\Input_data_deep\\Antennas_sd.txt",
-                            "planning_file_csv" :"D:\\Input_data_deep\\Planning_input.txt",
-                            "lte_carrier_file_csv":"D:\\Input_data_deep\\lte_carrier.txt",
-                            "GSI_file_xlsb" :"D:\\Input_data_deep\\SGI_input.xlsb",
-                            "out_put_data_dict_dir" :"D:\\out_dir",
-                            "profile_root_path": "D:\\Input_data_deep\\Ant Model"
-                        }""")
-                time.sleep(1)
-                raise KeyError
-            else:
-                return config_json_ob
+            except KeyError as profile_root_path_e:
+                raise profile_root_path_e
+            try:
+                out_put_data_dict_dir = config_json_ob["out_put_data_dict_dir"]
+                print(out_put_data_dict_dir)
+            except KeyError as out_put_data_dict_dir_e:
+                raise out_put_data_dict_dir_e
+            time.sleep(1)
+            return config_json_ob, planning_or_gis
 
-
-if __name__ == "__main__":
-    config_path = 'config\\config_phy.ini'
-    config_json_ob = read_configuration(config_path)
+# if __name__ == "__main__":
+#     config_path = 'config\\config_phy.ini'
+#     config_json_ob = read_configuration(config_path)
 
