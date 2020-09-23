0b4fc90cce95d0cf1613fd80934dbc39049a7e9e
swapan <sdbit04@gmail.com>
2020-09-24 00:21:40 +0530

    Improved packaging

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     setuptools.setup(
         entry_points={'console_scripts': ['phyDataPopulation=Physical_data_population.read_CLI_n_run:main_method',
                                           'phyDataPopulation_tmp=Physical_data_population.read_CLI_n_run_temp:main_method']},
-        install_requires=['pyxlsb', 'xlrd']
+        install_requires=['pyxlsb==1.0.5', 'xlrd'],
+        extras_require={'test': ['pytest>=3.7'], 'dev':[]}
     )
-
